7d9f13f7c3901b47ccc6c0efce90a488dd9b6ca1
Gilberto-Vera <rasvejr@gmail.com>
2019-10-09 17:37:03 -0300

    Primeiro Commit

---
+++ tutor/models.py
@@ -0,0 +1,238 @@
+from py2neo import Graph, Node, Relationship, NodeMatcher
+from datetime import datetime
+import uuid
+
+graph = Graph("http://localhost:7474", username="neo4j", password="neo4j1")
+matcher = NodeMatcher(graph)
+
+
+class Person:
+    def __init__(self, username):
+        self.username = username
+
+    def find(self):
+        user = matcher.match("Person", username__exact=self.username).first()
+        return user
+
+    def register(self, name, password, type):
+        if not self.find():
+            user = Node("Person", name=name, username=self.username, password=password, type=type)
+            graph.create(user)
+            return True
+        else:
+            return False
+
+    def verify_password(self, password):
+        user = self.find()
+        if user:
+            if password == user['password']:
+                return user
+            else:
+                return False
+        else:
+            return False
+
+    def add_post(self, title, tags, text):
+        user = self.find()
+        post = Node(
+            'Post',
+            id=str(uuid.uuid4()),
+            title=title,
+            text=text,
+            timestamp=timestamp(),
+            date=date()
+        )
+        rel = Relationship(user, 'PUBLISHED', post)
+        graph.create(rel)
+
+        tags = [x.strip() for x in tags.lower().split(',')]
+        for name in set(tags):
+            tag = Node("Tag", name=name)
+            #graph.merge(tag, "Tag", "name")
+
+            rel = Relationship(tag, 'TAGGED', post)
+            graph.create(rel)
+
+    def like_post(self, post_id):
+        user = self.find()
+        post = matcher.match('Post', id__exact=post_id).first()
+        graph.merge(Relationship(user, 'LIKED', post))
+
+    def get_recent_posts(self):
+        query = '''
+        MATCH (user:User)-[:PUBLISHED]->(post:Post)<-[:TAGGED]-(tag:Tag)
+        WHERE user.username = {username}
+        RETURN post, COLLECT(tag.name) AS tags
+        ORDER BY post.timestamp DESC LIMIT 5
+        '''
+
+        return graph.run(query, username=self.username)
+
+    def get_similar_users(self):
+        # Find three users who are most similar to the logged-in user
+        # based on tags they've both blogged about.
+        query = '''
+        MATCH (you:User)-[:PUBLISHED]->(:Post)<-[:TAGGED]-(tag:Tag),
+              (they:User)-[:PUBLISHED]->(:Post)<-[:TAGGED]-(tag)
+        WHERE you.username = {username} AND you <> they
+        WITH they, COLLECT(DISTINCT tag.name) AS tags
+        ORDER BY SIZE(tags) DESC LIMIT 3
+        RETURN they.username AS similar_user, tags
+        '''
+
+        return graph.run(query, username=self.username)
+
+    def get_commonality_of_user(self, other):
+        # Find how many of the logged-in user's posts the other user
+        # has liked and which tags they've both blogged about.
+        query = '''
+        MATCH (they:User {username: {they} })
+        MATCH (you:User {username: {you} })
+        OPTIONAL MATCH (they)-[:PUBLISHED]->(:Post)<-[:TAGGED]-(tag:Tag),
+                       (you)-[:PUBLISHED]->(:Post)<-[:TAGGED]-(tag)
+        RETURN SIZE((they)-[:LIKED]->(:Post)<-[:PUBLISHED]-(you)) AS likes,
+               COLLECT(DISTINCT tag.name) AS tags
+        '''
+
+        return graph.run(query, they=other.username, you=self.username).next
+
+
+class CourseClass:
+    def find(self, title):
+        cc = matcher.match("CourseClass", title__exact=title).first()
+        return cc
+
+    def create(self, title):
+        if not self.find(title):
+            cc = Node("CourseClass", title=title)
+            graph.create(cc)
+            return True
+        else:
+            return False
+
+    def delete(self, title):
+        if self.find(title):
+            cc = matcher.match("CourseClass", title__exact=title).first()
+            graph.delete(cc)
+            return True
+        else:
+            return False
+
+    def get_course_classes(self):
+        cc = matcher.match("CourseClass").order_by("_.title")
+        return cc
+
+
+class ClassSubject:
+    def find_in_course(self, cc, title):
+        query = '''
+                   MATCH (cs:ClassSubject)-[:TAUGHT]->(cc:CourseClass)
+                   WHERE cc.title = {cc} AND cs.title = {title}
+                   RETURN cs
+                   ORDER BY cs.order
+                   '''
+        cc = graph.run(query, cc=cc, title=title)
+        return cc
+
+    def create(self, course_class, title, ps, ns):
+        if not self.find_in_course(course_class, title).evaluate():
+            cc = CourseClass().find(course_class)
+
+            # TODO ClassSubject TEM QUE TER UM ATRIBUTO QUE DEFINE SE É O NÓ INICIAL DA DISCIPLINA.
+            # TODO SE FOR O PRIMEIRO NÓ DE UMA DISCIPLINA, AUTOMATICAMENTE É DEFINIDO COMO NÓ INICIAL.
+            # TODO POSTERIORMENTE, O USUÁRIO PODERÁ MUDAR QUAL O ASSUNTO (NÓ) INICIAL DE UMA DISCIPLINA.
+
+            cs = Node("ClassSubject", title=title)
+            graph.create(cs)
+
+            graph.merge(Relationship(cs, 'TAUGHT', cc))
+
+            if ps:
+                previous_subject = self.find_in_course(course_class, ps).evaluate()
+                graph.merge(Relationship(previous_subject, 'PREVIOUS_TO', cs))
+
+            if ns:
+                next_subject = self.find_in_course(course_class, ns).evaluate()
+                graph.merge(Relationship(cs, 'FORWARD_TO', next_subject))
+
+            return True
+        else:
+            return False
+
+    def get_class_subjects(self, title):
+
+        # TODO 1 - ORDER BY O QUÊ????? (OPCIONAL)
+        # TODO 2 - TEM QUE RETORNAR O TÍTULO DO ASSUNTO ANTERIOR E O TÍTULO DO ASSUNTO POSTERIOR, SE EXISTIREM
+        query = '''
+            MATCH (cs:ClassSubject)-[:TAUGHT]->(cc:CourseClass)
+            WHERE cc.title = {title}
+            RETURN cs, cc
+            '''
+        cc = graph.run(query, title=title)
+        return cc
+
+    def remove_class_subject(self):
+        #graph.delete()
+        return True
+
+
+class Question:
+    def find(self, id):
+        question = matcher.match("Question", id__exact=id).first()
+        return question
+
+    def create(self, cc, class_subject, title, body, difficulty, choice_a, choice_b, choice_c, choice_d, right_answer, user):
+        cs = ClassSubject().find_in_course(cc, class_subject).evaluate()
+
+        question = Node(
+            "Question",
+            id=str(uuid.uuid1()),
+            title=title,
+            body=body,
+            difficulty=difficulty,
+            choice_a=choice_a,
+            choice_b=choice_b,
+            choice_c=choice_c,
+            choice_d=choice_d,
+            right_answer=right_answer
+        )
+        graph.create(question)
+
+        u = Person(user).find()
+
+        graph.merge(Relationship(question, 'ASKED', cs))
+
+        graph.merge(Relationship(u, 'CREATED', question))
+        return True
+
+    def get_questions(self, cs_title, cc_title):
+        query = '''
+            MATCH (q:Question)-[:ASKED]->(cs:ClassSubject)-[:TAUGHT]->(cc:CourseClass)
+            WHERE cs.title = {cs_title} and cc.title = {cc_title}
+            RETURN q
+            ORDER BY q.title
+            '''
+        question = graph.run(query, cs_title=cs_title, cc_title=cc_title)
+        return question
+
+
+def get_todays_recent_posts():
+    query = '''
+    MATCH (user:Person)-[:PUBLISHED]->(post:Post)<-[:TAGGED]-(tag:Tag)
+    WHERE post.date = {today}
+    RETURN user.username AS username, post, COLLECT(tag.name) AS tags
+    ORDER BY post.timestamp DESC LIMIT 5
+    '''
+
+    return graph.run(query, today=date())
+
+
+def timestamp():
+    epoch = datetime.utcfromtimestamp(0)
+    now = datetime.now()
+    delta = now - epoch
+    return delta.total_seconds()
+
+
+def date():
+    return datetime.now().strftime('%Y-%m-%d')
---
+++ tutor/views.py
@@ -0,0 +1,261 @@
+from .models import Person, CourseClass, ClassSubject, Question, get_todays_recent_posts
+from flask import Flask, request, session, redirect, url_for, render_template, flash
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    # session['username'] = 'sirlon'
+    # session['type'] = 'teacher'
+    # posts = get_todays_recent_posts()
+    return render_template('index.html')
+
+
+# REGISTRO, LOGIN E LOGOUT #
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        name = request.form['name']
+        username = request.form['username']
+        password = request.form['password']
+        type = "student"
+
+        if len(username) < 1:
+            flash('Nome de usuário deve possuir pelo menos 1 caractere')
+        elif len(password) < 5:
+            flash('Senha deve ter pelo menos 5 caracteres')
+        elif not Person(username).register(name, password, type):
+            flash('Nome de usuário já existente')
+        else:
+            session['username'] = username
+            session['name'] = name
+            session['type'] = "student"
+            flash('Login efetuado com sucesso.')
+            return redirect(url_for('index'))
+
+    return render_template('register.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        user = Person(username).verify_password(password)
+        if not user:
+            flash('Nome de usuário ou senha incorretos')
+        else:
+            session['username'] = username
+            session['type'] = user['type']
+            session['person_name'] = user['name']
+            flash('Login efetuado com sucesso.')
+            return redirect(url_for('index'))
+
+    return render_template('login.html')
+
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    flash('Você saiu do sistema.')
+    return redirect(url_for('index'))
+
+
+# COURSE CLASS #
+@app.route('/open_course_class', methods=['GET', 'POST'])
+def open_course_class():
+    check_if_teacher()
+
+    course_classes = CourseClass().get_course_classes()
+    return render_template(
+        'course_class.html',
+        cc=course_classes
+    )
+
+
+@app.route('/create_course_class', methods=['GET', 'POST'])
+def create_course_class():
+    check_if_teacher()
+
+    if request.method == 'POST':
+        title = request.form['title']
+
+        if not CourseClass().create(title):
+            flash('Disciplina já existente')
+        else:
+            flash('Disciplina criada com sucesso.')
+
+    return redirect(request.referrer)
+
+
+@app.route('/delete_course_class/<title>')
+def delete_course_class(title):
+    check_if_teacher()
+
+    if not CourseClass().delete(title):
+        flash('Disciplina não encontrada')
+    else:
+        flash('Disciplina excluida com sucesso.')
+
+    return redirect(request.referrer)
+
+
+# QUESTIONS #
+@app.route('/open_questions/<cs_title>/<cc_title>')
+def open_questions(cs_title, cc_title):
+    check_if_teacher()
+
+    questions = list(Question().get_questions(cs_title, cc_title))
+
+    return render_template(
+        'question.html',
+        cs=cs_title,
+        cc=cc_title,
+        q=questions
+    )
+
+
+@app.route('/create_question', methods=['GET', 'POST'])
+def create_question():
+    check_if_teacher()
+
+    if request.method == 'POST':
+        cs = request.form['cs']
+        cc = request.form['cc']
+        title = request.form['question_title']
+        body = request.form['question_body']
+        difficulty = request.form['difficulty']
+        choice_a = request.form['choice_a']
+        choice_b = request.form['choice_b']
+        choice_c = request.form['choice_c']
+        choice_d = request.form['choice_d']
+        right_answer = request.form['right_answer']
+
+        if not Question().create(cc, cs, title, body, difficulty, choice_a, choice_b, choice_c, choice_d, right_answer,
+                                 session["username"]):
+            flash('Erro ao cadastrar questão')
+        else:
+            flash('Questão criada com sucesso.')
+
+    return redirect(request.referrer)
+
+
+@app.route('/delete_question/<id>', methods=['GET', 'POST'])
+def delete_question():
+    check_if_teacher()
+
+
+# CLASS SUBJECT #
+@app.route('/open_class_subject/<title>')
+def open_class_subject(title):
+    check_if_teacher()
+
+    class_subjects = list(ClassSubject().get_class_subjects(title))
+
+    return render_template(
+        'class_subject.html',
+        cc=title,
+        cs=class_subjects
+    )
+
+
+@app.route('/remove_class_subject/<title>')
+def remove_class_subject(title):
+    check_if_teacher()
+
+    ClassSubject().remove_class_subject()
+
+    return render_template('open_class_subject.html')
+
+
+@app.route('/create_class_subject', methods=['GET', 'POST'])
+def create_class_subject():
+    check_if_teacher()
+
+    if request.method == 'POST':
+        title = request.form['subject_title']
+        cc = request.form['cc']
+        ps = request.form.get('previous_subject')
+        ns = request.form.get('next_subject')
+
+        if len(title) < 1:
+            flash('O assunto deve possuir pelo menos 1 caractere')
+        elif not ClassSubject().create(cc, title, ps, ns):
+            flash('Assunto já existente')
+        else:
+            flash('Assunto criado com sucesso.')
+
+    return redirect(request.referrer)
+
+
+# MÉTODOS LEGADOS DO EXEMPLO #
+@app.route('/add_post', methods=['POST'])
+def add_post():
+    title = request.form['title']
+    tags = request.form['tags']
+    text = request.form['text']
+
+    if not title:
+        flash('You must give your post a title.')
+    elif not tags:
+        flash('You must give your post at least one tag.')
+    elif not text:
+        flash('You must give your post a text body.')
+    else:
+        Person(session['username']).add_post(title, tags, text)
+
+    return redirect(url_for('index'))
+
+
+@app.route('/like_post/<post_id>')
+def like_post(post_id):
+    username = session.get('username')
+
+    if not username:
+        flash('You must be logged in to like a post.')
+        return redirect(url_for('login'))
+
+    Person(username).like_post(post_id)
+
+    flash('Liked post.')
+    return redirect(request.referrer)
+
+
+@app.route('/profile/<username>')
+def profile(username):
+    logged_in_username = session.get('username')
+    user_being_viewed_username = username
+
+    user_being_viewed = Person(user_being_viewed_username)
+    posts = user_being_viewed.get_recent_posts()
+
+    similar = []
+    common = []
+
+    if logged_in_username:
+        logged_in_user = Person(logged_in_username)
+
+        if logged_in_user.username == user_being_viewed.username:
+            similar = logged_in_user.get_similar_users()
+        else:
+            common = logged_in_user.get_commonality_of_user(user_being_viewed)
+
+    return render_template(
+        'profile.html',
+        username=username,
+        posts=posts,
+        similar=similar,
+        common=common
+    )
+
+
+# VERIFICA SE É PROFESSOR #
+def check_if_teacher():
+    username = session.get('username')
+    t = session.get('type')
+
+    if not username and t != 'teacher':
+        flash('Você não está logado como professor.')
+        return redirect(url_for('login'))
